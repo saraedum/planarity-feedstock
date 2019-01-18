2ef23b18e2306955be7728cb701bc5d97291aa4c
Isuru Fernando <isuruf@gmail.com>
2019-01-18 14:29:42 -0600

    Build dll only on vc14

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,10 +7,17 @@ REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
 REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
 set CFLAGS=%CFLAGS:-GL=%
 
+if %VS_MAJOR% == 9 (
+    set "BUILD_SHARED_LIBS=no"
+) else (
+    set "BUILD_SHARED_LIBS=yes"
+)
+
 cmake -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_SHARED_LIBS=%BUILD_SHARED_LIBS% ^
       ..
 
 cmake --build . --target install --config Release
