b1195dc7a507e17db83a3eaab3b36ce93a51070a
Isuru Fernando <isuruf@gmail.com>
2019-01-17 23:02:48 -0600

    Remove -GL

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,10 @@ copy "%RECIPE_DIR%\CMakeLists.txt" .
 mkdir build
 cd build
 
+REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
+REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
+set CFLAGS=%CFLAGS:-GL=%
+
 cmake -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
